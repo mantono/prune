9f1544e63b839f1157b1cd11f7473b29ecd0c48d
Anton Österberg <anton.osterberg@gmail.com>
2019-12-18 19:28:14 +0100

    WIP: Trying to chain iterators, removing the need for for-loop

--- src/find.rs
+++ src/find.rs
@@ -4,23 +4,25 @@ use std::fs::{ReadDir, Metadata};
 use std::fs;
 use std::collections::VecDeque;
 
-pub trait SideEffect {
-    fn submit(&mut self, file: &PathBuf);
-}
-
-pub fn explore(current_dir: PathBuf, max_depth: u32, find: u64, min_size: u64, fc: &mut dyn SideEffect) -> (u64, u64) {
-    let files: Vec<u64> = FileExplorer::for_path(&current_dir, max_depth)
+pub fn explore(current_dir: PathBuf, max_depth: u32, find: u64, min_size: u64) -> (u64, u64) {
+    let files: Vec<&PathBuf> = FileExplorer::for_path(&current_dir, max_depth)
         .filter(|f: &PathBuf| filter_size(f, min_size))
         .take(find as usize)
-        .map(|f| f.canonicalize().expect("Unable to get canonical path"))
-        .inspect(|f| fc.submit(f))
-        .map(|f| f.metadata().unwrap().len())
         .collect();
 
-    (files.len() as u64, files.iter().sum())
+    summarize(files)
 }
 
-struct FileExplorer {
+pub fn summarize(files: Vec<&PathBuf>) -> (u64, u64) {
+    let found: u64 = files.len() as u64;
+    let size: u64 = files.iter()
+        .map(|f| f.metadata().unwrap().len())
+        .sum();
+
+    (found, size)
+}
+
+pub struct FileExplorer {
     files: VecDeque<PathBuf>,
     dirs: VecDeque<PathBuf>,
     origin: PathBuf,
@@ -41,6 +43,15 @@ impl FileExplorer {
             dirs,
             origin: path.clone(),
             max_depth
+        }
+    }
+
+    pub fn empty() -> FileExplorer {
+        FileExplorer {
+            files: VecDeque::with_capacity(0),
+            dirs: VecDeque::with_capacity(0),
+            origin: PathBuf::from("/dev/null"),
+            max_depth: 0
         }
     }
 
@@ -127,65 +138,35 @@ fn is_symlink(path: &PathBuf) -> bool {
 #[cfg(test)]
 mod tests {
     use std::path::PathBuf;
-    use crate::find::explore;
-    use crate::find::tests::cf::Saver;
+    use crate::find::{explore, FileExplorer};
 
     #[test]
     fn test_depth_only_root_dir() {
-        let mut save = Saver::new(10);
         let current_dir = PathBuf::from("test_dirs");
-        let result: (u64, u64) = explore(current_dir, 0, 100, 1, &mut save);
+        let result: (u64, u64) = explore(current_dir, 0, 100, 1);
         assert_eq!(1, result.0);
-        assert_eq!("file0", save.files.first().unwrap().file_name().unwrap());
     }
 
     #[test]
     fn test_depth_one() {
-        let mut save = Saver::new(10);
         let current_dir = PathBuf::from("test_dirs");
-        let result: (u64, u64) = explore(current_dir, 1, 100, 1, &mut save);
+        let result: (u64, u64) = explore(current_dir, 1, 100, 1);
         assert_eq!(3, result.0);
         assert_eq!(182, result.1);
     }
 
     #[test]
     fn test_stop_at_one_found_file() {
-        let mut save = Saver::new(5);
         let current_dir = PathBuf::from("test_dirs");
-        let result: (u64, u64) = explore(current_dir, 3, 1, 1, &mut save);
+        let result: (u64, u64) = explore(current_dir, 3, 1, 1);
         assert_eq!(1, result.0);
     }
 
     #[test]
     fn test_filter_by_file_size() {
-        let mut save = Saver::new(5);
         let current_dir = PathBuf::from("test_dirs");
-        let result: (u64, u64) = explore(current_dir, 3, 10, 100, &mut save);
+        let result: (u64, u64) = explore(current_dir, 3, 10, 100);
         assert_eq!(1, result.0);
         assert_eq!(100, result.1);
-        assert_eq!("file2", save.files.first().unwrap().file_name().unwrap());
-    }
-
-    mod cf {
-        use std::path::PathBuf;
-        use crate::find::SideEffect;
-
-        pub struct Saver {
-            pub files: Vec<PathBuf>
-        }
-
-        impl Saver {
-            pub fn new(capacity: usize) -> Saver {
-                Saver {
-                    files: Vec::with_capacity(capacity)
-                }
-            }
-        }
-
-        impl SideEffect for Saver {
-            fn submit(&mut self, file: &PathBuf) {
-                self.files.push(file.to_path_buf())
-            }
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,35 @@ mod args;
 
 use humansize::{FileSize, file_size_opts as options};
 use std::path::PathBuf;
-use crate::find::{explore, SideEffect};
+use crate::find::{FileExplorer, summarize};
 use crate::cfg::Config;
+use std::result::Iter;
+
+type FileIterator = dyn Iterator<Item=PathBuf>;
 
 fn main() {
     let cfg: Config = Config::from_args(args::args());
-    let mut limit: u64 = cfg.limit as u64;
 
-    let (found, sizes) = cfg.paths.iter()
-        .map(|p| {
-            let current_dir = PathBuf::from(p);
-            let result: (u64, u64) = explore(current_dir, cfg.max_depth, limit, cfg.min_size, &mut PrintFile);
-            limit -= result.0;
-            result
-        })
-        .unzip::<u64, u64, Vec<u64>, Vec<u64>>();
+    let files: Vec<&PathBuf> = cfg.paths.iter()
+        .map(|p| PathBuf::from(p))
+        .map(|path: PathBuf| FileExplorer::for_path(&path, cfg.max_depth))
+        .fold(std::iter::empty::<PathBuf>(), merge)
+        .take(cfg.limit)
+        .inspect(|f| print(f))
+        .collect();
 
-    let sum_found: u64 = found.iter().sum();
-    let sum_size: u64 = sizes.iter().sum();
-    let human_size = sum_size.file_size(options::CONVENTIONAL).unwrap();
-    println!("Found {} files with a total size of {}", sum_found, human_size);
+    let (found, size) = summarize(files);
+
+    let human_size = size.file_size(options::CONVENTIONAL).unwrap();
+    println!("Found {} files with a total size of {}", found, human_size);
 }
 
-struct PrintFile;
+fn merge(i0: impl Iterator<Item=PathBuf>, i1: impl Iterator<Item=PathBuf>) -> impl Iterator<Item=PathBuf> {
+    i0.chain(i1)
+}
 
-impl SideEffect for PrintFile {
-    fn submit(&mut self, file: &PathBuf) {
-        let canon: PathBuf = file.canonicalize().expect("Unable to get canonical path");
-        let size = file.metadata().unwrap().len().file_size(options::CONVENTIONAL).unwrap();
-        println!("{}, {:?}", size, canon)
-    }
+fn print(file: &PathBuf) {
+    let canon: PathBuf = file.canonicalize().expect("Unable to get canonical path");
+    let size = file.metadata().unwrap().len().file_size(options::CONVENTIONAL).unwrap();
+    println!("{}, {:?}", size, canon)
 }
